a6e844b51685f5fbe440bffcf0033da743fb07b6
GerardoCoronel98 <tinococoronel.gerardo@gmail.com>
2023-11-24 16:10:54 -0600

    Post Evaluaciones & Estudiantes

--- index.js
+++ index.js
@@ -3,35 +3,52 @@ const app = express();
 const bd = require('./config/database');
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 
 app.post('/', async (req, res, next) => {
 
-    const { expediente, nombre, apellidos, carrera, correo, telefono} = req.body;
-    
-    console.log(req.body)
+    const { expediente, nombre, apellidos, carrera, correo, telefono, nip } = req.body;
 
+    if (expediente && nombre && apellidos && carrera && correo && telefono && nip) {
 
-    if(expediente && nombre && apellidos && carrera && correo && telefono && NIP) {
-        
-        let query = "INSERT INTO estudiantes (expediente, nombre, apellidos, carrera, correo, telefono, NIP) "
-        query += `VALUES ('${expediente}', '${nombre}','${apellidos}','${carrera}','${correo}','${telefono}', '${NIP}')`;
+        let query = "INSERT INTO estudiantes (expediente, nombre, apellidos, carrera, correo, telefono, nip) "
+        query += `VALUES ('${expediente}', '${nombre}','${apellidos}','${carrera}','${correo}','${telefono}', '${nip}')`;
         const rows = await bd.query(query)
 
-        if (rows.affectedRows == 1){
-            return res.status(201).json({code: 201, message: "Estudiante agregado correctamente"})
+        if (rows.affectedRows == 1) {
+            return res.status(201).json({ code: 201, message: "Estudiante agregado correctamente" })
         }
-        return res.status(500).json({code: 500, message: "Ocurrio un error"})
+        return res.status(500).json({ code: 500, message: "Ocurrio un error" })
     }
 
-    return res.status(500).json({code: 500, message: "Datos incompletos"})
+    return res.status(500).json({ code: 500, message: "Datos incompletos" })
 
 
 })
 
+app.post('/evaluacion', async (req, res, next) => {
 
-app.get("/",(req, res, next) =>{
+    const { nombre_area, evaluacion_area } = req.body;
+    console.log(req.body)
+
+    if (nombre_area && evaluacion_area) {
+
+        let query = "INSERT INTO evaluaciones(nombre_area, evaluacion_area) "
+        query += `VALUES ('${nombre_area}','${evaluacion_area}')`
+        const rows = await bd.query(query)
+
+        if (rows.affectedRows == 1) {
+            return res.status(201).json({ code: 201, message: "Evaluación agregada correctamente" })
+        }
+        return res.status(500).json({ code: 500, message: "Ocurrio un error" })
+    }
+
+    return res.status(500).json({ code: 500, message: "Datos incompletos" })
+
+})
+
+app.get("/", (req, res, next) => {
     res.status(200);
     res.send("Bienvenido")
 });
